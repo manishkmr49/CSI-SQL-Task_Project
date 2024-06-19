e515a653b811ac451097354b6df9a9da551065ce
Manish Kumar <110106315+manishkmr49@users.noreply.github.com>
2024-06-19 23:30:15 +0530

    Delete Assignment - Level C Task directory

--- Assignment - Level C Task/Task 17.sql
+++
@@ -1,8 +0,0 @@
-USE AdventureWorks2019;
-
-CREATE LOGIN Celebal WITH PASSWORD = 'Kumar@123';
-
-CREATE USER Manish FOR LOGIN Celebal;
-
--- Provide permissions of DB_OWNER to the user
-ALTER ROLE db_owner ADD MEMBER Manish;
